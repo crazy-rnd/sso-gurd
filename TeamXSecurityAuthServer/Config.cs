cfee181fcd01742a62e50e22e02e4cf3bbdbc291
Rakibul Hasan <rakibul.hasan@LEADSOFT.BIZ>
2019-06-13 15:55:24 +0600

    First commit

---
+++ TeamXSecurityAuthServer/Config.cs
@@ -0,0 +1,33 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TeamXSecurityAuthServer
+{
+    public class Config
+    {
+        public static IEnumerable<Client> GetClients()
+        {
+            // client credentials, list of clients
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+ 
+                    // Client secrets
+                    ClientSecrets ={ new Secret ("secret".Sha256()) },
+                    AllowedScopes = { "api1" }
+                },
+            };
+        }
+
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>{ new ApiResource("api1", "My API") };
+        }
+    }
+}
